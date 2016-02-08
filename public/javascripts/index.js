310f92519ae33061770d2bc51c2adc1d80f1f227
ZHKO1 <zhko11993@gmail.com>
2016-02-08 10:22:34 +0800

    暂且日历部分调试完毕

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,10 +3,9 @@
  */
 
 //Todo 规划好界面 模仿github的界面来尝试 可以在学习纪录功能 以及 PSN奖杯查看 界面切换
-
-//Todo 规划前端模版
-
 //Todo 熟悉了解日历插件的用法
+//Todo 了解熟悉日历插件的写法，能尽力改善就改善就改善
+//
 var cal = new CalHeatMap();
 var startDate = new Date();
 var day = startDate.getDate()
@@ -18,7 +17,6 @@ cal.init({
   cellSize: 11.5,
   range: 1,
   displayLegend: false,
-  start: new Date(2015, 3, 7),
-  end: new Date(),
+  start: new Date(),
   tooltip: true
 });
